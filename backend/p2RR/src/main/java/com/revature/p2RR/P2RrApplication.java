b68058fad6b312f5c074219c134e92ce34f12089
KarlitosTaquitos <chern4261@gmail.com>
2022-03-24 13:26:45 -0500

    Initial commit
     - Created spring app in backend folder
     - Created angular app in frontend folder

---
+++ backend/p2RR/src/main/java/com/revature/p2RR/P2RrApplication.java
@@ -0,0 +1,13 @@
+package com.revature.p2RR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2RrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2RrApplication.class, args);
+	}
+
+}
---
+++ backend/p2RR/src/test/java/com/revature/p2RR/P2RrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.p2RR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2RrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
