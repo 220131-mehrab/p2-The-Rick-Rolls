ca4c0020f438901511840921281dca824b237b60
KarlitosTaquitos <chern4261@gmail.com>
2022-03-29 11:37:07 -0500

    Tests are back and use an in-memory h2 database

--- backend/src/test/java/com/revature/p2RR/P2RrApplicationTests.java
+++ backend/src/test/java/com/revature/p2RR/P2RrApplicationTests.java
@@ -2,8 +2,14 @@ package com.revature.p2RR;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class P2RrApplicationTests {
 
+    @Test
+    void contextLoads() {
+        
+    }
 }
