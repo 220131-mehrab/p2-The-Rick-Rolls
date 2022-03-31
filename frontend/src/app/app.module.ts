0388cfa25b51b9b0b513a4b7c65352e8dae63a63
Skipuavforge <kevinsonlineshopping@verizon.net>
2022-03-31 15:52:05 -0400

    Rework of frontend before copy back of temporarily removed files.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MyMapComponent } from './my-map/my-map.component';
-import { UserComponent } from './user/user.component';
-import { WaypointComponent } from './waypoint/waypoint/waypoint.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyMapComponent,
-    UserComponent,
-    WaypointComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
