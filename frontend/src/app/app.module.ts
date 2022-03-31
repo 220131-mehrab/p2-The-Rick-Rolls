65e9f05b85a0906aada58d0976511012e47c2289
Skipuavforge <kevinsonlineshopping@verizon.net>
2022-03-31 16:22:04 -0400

    Removal of extra package.json.  addition of generated user and waypoint classes.  Replacement of my-app stuff. Didn't replace application file with 'p2-angular' title.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyMapComponent } from './my-map/my-map.component';
+import { UserComponent } from './user/user.component';
+import { WaypointComponent } from './waypoint/waypoint/waypoint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyMapComponent,
+    UserComponent,
+    WaypointComponent
   ],
   imports: [
     BrowserModule
