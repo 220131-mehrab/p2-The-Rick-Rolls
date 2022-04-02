937eb5719134f36268989a800e0f76ebaf1eb4db
brian <brian.w.727@gmail.com>
2022-04-02 10:56:42 -0700

    map needs #map in map div tag. finally displays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare const L: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'p2-beta';
+export class AppComponent implements OnInit {
+  title = 'Wayfinder';
+
+  ngOnInit() {
+    if (!navigator.geolocation) {
+      console.log('sorry, cant find you!');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = position.coords;
+
+      console.log(
+        `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+      );
+      let map = L.map('map').setView([coords.latitude, coords.longitude], 4);
+    });
+    this.watchPosition();
+  }
+
+  watchPosition() {
+    let desLat = 0;
+    let desLon = 0;
+    let id = navigator.geolocation.watchPosition((position) => {
+      console.log(
+      `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+      );
+      if (position.coords.latitude === desLat) {
+        navigator.geolocation.clearWatch(id);
+      }
+  },(err) => {
+    console.log(err);
+  },{
+    enableHighAccuracy: false,
+    timeout: 5000,
+    //set to 0 so device cant return cached postion
+    maximumAge: 0
+  })
+}
 }
