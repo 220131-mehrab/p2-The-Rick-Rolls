d2ae9d4f2d275782ea8c4b54b8607108c25c5bec
brian <brian.w.727@gmail.com>
2022-04-02 11:13:01 -0700

    map layer added with coordinate reading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+//makes L layer a global js variable
 declare const L: any;
 @Component({
   selector: 'app-root',
@@ -19,7 +19,17 @@ export class AppComponent implements OnInit {
       console.log(
         `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
       );
+      //adding leaflet map canvas to map div
       let map = L.map('map').setView([coords.latitude, coords.longitude], 4);
+      //adds map layer to leaflet canvas, include access token here
+      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJpYW4xMzQ4IiwiYSI6ImNrNzlqcjl3NjB1MHkzZG10bjBscnp4NmgifQ.vieWJC5E5Odyimr9smQaIQ', {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token'
+    }).addTo(map);
     });
     this.watchPosition();
   }
