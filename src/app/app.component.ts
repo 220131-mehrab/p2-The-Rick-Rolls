08d9f5d43810afd131b3725b012890e78bd073a1
brian <brian.w.727@gmail.com>
2022-04-02 11:51:12 -0700

    fixed leaflet bug with marker icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 //makes L layer a global js variable
 declare const L: any;
+//fix for leaflet icon marker bug
+import { icon, Marker } from 'leaflet';
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+Marker.prototype.options.icon = iconDefault;
+//end leaflet bug fix
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -32,6 +51,7 @@ export class AppComponent implements OnInit {
           zoomOffset: -1,
           accessToken: 'your.mapbox.access.token'
       }).addTo(map);
+      
       //marker 
       let marker = L.marker(latLong).addTo(map);
       //add popup
