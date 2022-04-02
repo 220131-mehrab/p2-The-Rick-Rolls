80dc101304a9d5b2ea461f1dfc476530f23973dc
brian <brian.w.727@gmail.com>
2022-04-01 21:10:47 -0700

    implementing navigator.geolocation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
-import { Map, NavigationControl } from 'maplibre-gl';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Project Two: Wayfinder';
+export class AppComponent implements OnInit {
+  title = 'Wayfinder';
+  
+  ngOnit() { 
+    if (!navigator.geolocation) {
+      console.log('sorry, we cant find you');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(
+        `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}` 
+        + "this is your lat and long"
+      );
+    });
+  }
+
+
+  watchPosition() {
+    
+  }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-import { WaypointsComponent } from './waypoints/waypoints.component';
-import { MyMapComponent } from './my-map/my-map.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyMapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
