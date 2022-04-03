17c8532943a19a3bb7ae116f5f03a3e17f52cddd
Skipuavforge <kevinsonlineshopping@verizon.net>
2022-04-03 11:23:36 -0400

    added (click) to appcomponent.HTML.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,49 +32,51 @@ export class AppComponent implements OnInit {
   mylon = 0;
 
   ngOnInit(): void {
-      this.event();
+      //this.event();
   }
 
   event() {
     console.log("I pressed the button.")
     if (!navigator.geolocation) {
       console.log('sorry, cant find you!');
+    }else{
+      navigator.geolocation.getCurrentPosition((position) => {
+        //simplifies coords
+        const coords = position.coords;
+        const latLong = [coords.latitude, coords.longitude];
+
+        console.log(
+          `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+        );
+        this.mylat = position.coords.latitude;
+        this.mylon = position.coords.longitude;
+
+        
+        //adding leaflet map canvas to map div
+        let map = L.map('map').setView(latLong, 4);
+        //adds map layer to leaflet canvas, include access token here
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJpYW4xMzQ4IiwiYSI6ImNrNzlqcjl3NjB1MHkzZG10bjBscnp4NmgifQ.vieWJC5E5Odyimr9smQaIQ', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'your.mapbox.access.token'
+        }).addTo(map);
+
+        //marker 
+        let marker = L.marker(latLong).addTo(map);
+        //add popup
+        // marker.bindPopup('<b>Brian lives here</b>')
+      //   let popup = L.popup()
+      // .setLatLng(latLong)
+      // .setContent("Waypoint One")
+      // .openOn(map);
+      });
     }
-    navigator.geolocation.getCurrentPosition((position) => {
-      //simplifies coords
-      const coords = position.coords;
-      const latLong = [coords.latitude, coords.longitude];
-
-      console.log(
-        `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
-      );
-      this.mylat = position.coords.latitude;
-      this.mylon = position.coords.longitude;
-
-      
-      //adding leaflet map canvas to map div
-      let map = L.map('map').setView(latLong, 4);
-      //adds map layer to leaflet canvas, include access token here
-      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJpYW4xMzQ4IiwiYSI6ImNrNzlqcjl3NjB1MHkzZG10bjBscnp4NmgifQ.vieWJC5E5Odyimr9smQaIQ', {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox/streets-v11',
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken: 'your.mapbox.access.token'
-      }).addTo(map);
-
-      //marker 
-      let marker = L.marker(latLong).addTo(map);
-      //add popup
-      // marker.bindPopup('<b>Brian lives here</b>')
-    //   let popup = L.popup()
-    // .setLatLng(latLong)
-    // .setContent("Waypoint One")
-    // .openOn(map);
-    });
     this.watchPosition();
   }
+
 
   watchPosition() {
     let desLat = 0;
