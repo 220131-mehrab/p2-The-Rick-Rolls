e14a0735eb162e05af3081a7b394c329718f23de
KarlitosTaquitos <chern4261@gmail.com>
2022-04-03 21:33:26 -0500

    cleaned up folders

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WaypointsComponent } from './waypoints/waypoints.component';
+import { WayptDetailComponent } from './waypt-detail/waypt-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WaypointsComponent,
+    WayptDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
